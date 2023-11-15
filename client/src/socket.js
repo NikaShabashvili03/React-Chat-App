769577bec705cbe5d0a798705405f4c3172377ab
Nika Shabashvili <nikashabashvili@Nikas-MacBook-Pro.local>
2023-11-15 08:52:41 +0400

    ncom

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const URL = "https://reactchatapp-lzua.onrender.com";
+const URL = "https://reactchatapp-j3ch.onrender.com";
 
 export const socket = io(URL);
